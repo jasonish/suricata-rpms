ff911f26c96d0bf985e4ced3a7835bd5f1691669
Jason Ish <ish@unx.ca>
2020-04-28 10:15:29 -0600

    common.mk: add copr-testing target
    To push to copr-testing repos instead of the primary
    ones to test builds.

--- common.mk
+++ common.mk
@@ -1,6 +1,15 @@
-copr-build:
+srpm:
 	fedpkg --name $(NAME) --dist epel7 srpm
+
+copr-build: srpm
 	copr build $(COPR_REPO) $(NAME)*.el7.src.rpm
+
+copr-testing: srpm
+	copr build $(COPR_REPO)-testing $(NAME)*.el7.src.rpm
+
+update-sources:
+	spectool -g suricata.spec
+	sha512sum --tag suricata-*.tar.gz > sources
 
 $(DISTS):
 	fedpkg --name $(NAME) --dist $@ mockbuild
